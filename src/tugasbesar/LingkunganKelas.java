f550250c4d6ac89b549e419b6e9cb2ac2564aa31
abimsfardiansa <abims@gmx.com>
2015-05-21 09:18:24 +0700

    Update LingkunganKelas.java
    inherintance

--- src/tugasbesar/LingkunganKelas.java
+++ src/tugasbesar/LingkunganKelas.java
@@ -2,28 +2,123 @@ package tugasbesar;
 
 import java.util.Scanner;
 
-public class LingkunganKelas{
-    lingkungankelassetget sid = new lingkungankelassetget();
+public class LingkunganKelas extends RuangKelas{
+    
     Scanner sob = new Scanner(System.in);
     
-    public void lingkungan(){
-        System.out.println("Kondisi lantai (bersih/kotor) = ");
-        sid.setKondisilantai(sob.nextLine());
-        System.out.println("Kondisi dinding (bersih/kotor) = ");
-        sid.setKondisidinding(sob.nextLine());
-        System.out.println("Kondisi atap (bersih/kotor) = ");
-        sid.setKondisiatap(sob.nextLine());
-        System.out.println("Kondisi pintu (bersih/kotor) = ");
-        sid.setKondisipintu(sob.nextLine());
-        System.out.println("Kondisi jendela (bersih/kotor) = ");
-        sid.setKondisijendela(sob.nextLine());
+    private String kondisilantai;
+    private String kondisidinding;
+    private String kondisiatap;
+    private String kondisipintu;
+    private String kondisijendela;
+    
+    
+    
+    public String getKondisilantai() {
+        return kondisilantai;
+    }
+
+    public void setKondisilantai(String kondisilantai) {
+        this.kondisilantai = kondisilantai;
+        String kondlantai = sob.nextLine();
+		if("Bersih".equals(kondlantai) || "bersih".equals(kondlantai))
+			System.out.println("Sesuai");
+		else if("Kotor".equals(kondlantai) || "kotor".equals(kondlantai))
+			System.out.println("Tidak Sesuai");
+		else 
+			System.out.println("Input Salah");
+    }
+
+    public String getKondisidinding() {
+        return kondisidinding;
+    }
+
+    public void setKondisidinding(String kondisidinding) {
+        this.kondisidinding = kondisidinding;
+        String konddinding = sob.nextLine();
+		if("Bersih".equals(konddinding) || "bersih".equals(konddinding))
+			System.out.println("Sesuai");
+		else if("Kotor".equals(konddinding) || "kotor".equals(konddinding))
+			System.out.println("Tidak Sesuai");
+		else 
+			System.out.println("Input Salah");
+    }
+
+    public String getKondisiatap() {
+        return kondisiatap;
+    }
+
+    public void setKondisiatap(String kondisiatap) {
+        this.kondisiatap = kondisiatap;
+        String kondatap = sob.nextLine();
+		if("Bersih".equals(kondatap) || "bersih".equals(kondatap))
+			System.out.println("Sesuai");
+		else if("Kotor".equals(kondatap) || "kotor".equals(kondatap))
+			System.out.println("Tidak Sesuai");
+		else 
+			System.out.println("Input Salah");
+    }
+
+    public String getKondisipintu() {
+        return kondisipintu;
+    }
+
+    public void setKondisipintu(String kondisipintu) {
+        this.kondisipintu = kondisipintu;
+        String kondpintu = sob.nextLine();
+		if("Bersih".equals(kondpintu) || "bersih".equals(kondpintu))
+			System.out.println("Sesuai");
+		else if("Kotor".equals(kondpintu) || "kotor".equals(kondpintu))
+			System.out.println("Tidak Sesuai");
+		else 
+			System.out.println("Input Salah");
+    }
+
+    public String getKondisijendela() {
+        return kondisijendela;
+    }
+
+    public void setKondisijendela(String kondisijendela) {
+        this.kondisijendela = kondisijendela;
+        String kondjendela = sob.nextLine();
+		if("Bersih".equals(kondjendela) || "bersih".equals(kondjendela))
+			System.out.println("Sesuai");
+		else if("Kotor".equals(kondjendela) || "kotor".equals(kondjendela))
+			System.out.println("Tidak Sesuai");
+		else 
+			System.out.println("Input Salah");
     }
     
-    public void hasillingkungan(){
-        System.out.println("\nLantai = "+sid.getKondisilantai());
-        System.out.println("Dinding = "+sid.getKondisidinding());
-        System.out.println("Atap = "+sid.getKondisiatap());
-        System.out.println("Pintu = "+sid.getKondisipintu());
-        System.out.println("Jendela = "+sid.getKondisijendela());
+    
+    
+    
+    //public void lingkungan(){
+   // @Override
+    public void inputan(){
+        System.out.println("Kondisi lantai (bersih/kotor) = ");
+        String fa=sob.next();
+        setKondisilantai(fa);
+        System.out.println("Kondisi dinding (bersih/kotor) = ");
+        String fb=sob.next();
+        setKondisidinding(fb);
+        System.out.println("Kondisi atap (bersih/kotor) = ");
+        String fc=sob.next();
+        setKondisiatap(fc);
+        System.out.println("Kondisi pintu (bersih/kotor) = ");
+        String fd=sob.next();
+        setKondisipintu(fd);
+        System.out.println("Kondisi jendela (bersih/kotor) = ");
+        String fe=sob.next();
+        setKondisijendela(fe);
+    }
+    
+    //public void hasillingkungan(){
+   // @Override
+    public void outputan(){
+        System.out.println("\nLantai = "+getKondisilantai());
+        System.out.println("Dinding = "+getKondisidinding());
+        System.out.println("Atap = "+getKondisiatap());
+        System.out.println("Pintu = "+getKondisipintu());
+        System.out.println("Jendela = "+getKondisijendela());
     }
 }
